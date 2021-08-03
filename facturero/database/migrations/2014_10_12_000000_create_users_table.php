c8237f55dea873dd10e74353ec93c4a36a1bd683
david <davinsaca@yahoo.com>
2021-08-02 23:19:03 -0500

    Login and register

--- facturero/database/migrations/2014_10_12_000000_create_users_table.php
+++ facturero/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('role');
+            $table->string('role')->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('tipo_usuario')->default(0); // 0 = suscriptor && 1 = administrador
             $table->rememberToken();
             $table->timestamps();
         });
